9b83bcd15c7034973e525438d7715dd8e6fba82f
AwakeDC <112108382+AwakeDC@users.noreply.github.com>
2022-09-11 11:34:07 +0300

    Update endedGiveawayReactionAdded.js

--- events/giveaways/endedGiveawayReactionAdded.js
+++ events/giveaways/endedGiveawayReactionAdded.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js")
 module.exports = {
   async execute(giveaway, member, reaction){
      reaction.users.remove(member.user);
-  member.send(`**Aw snap! Looks Like that giveaway has already ended!**`).catch(e => {})
+  member.send(`**Hay aksi! Görünüşe göre bu çekiliş çoktan sona erdi!**`).catch(e => {})
   }
 }
